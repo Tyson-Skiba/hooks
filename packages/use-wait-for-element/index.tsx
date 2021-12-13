9eba69ffe589f72441fa7b7b274fa17ad09e97ca
Tyson Skiba <tyson@skiba.com.au>
2021-12-13 20:42:43 +1100

    feat(use-wait-for-element): created hook for waiting for an element to be available on the dom

---
+++ packages/use-wait-for-element/index.tsx
@@ -0,0 +1,31 @@
+import { useEffect, DependencyList } from 'react';
+
+type Selector = Parameters<ParentNode['querySelector']>[0];
+
+/**
+ * Run callback when element is visible on the dom
+ * @param {Selector} selector - HTML selector to find dom element.
+ * @param {function} callback - Function that is called when node appears on the dom.
+ * @param {DependencyList} dependencies - Cache key.
+ */
+export const useWaitForElement = (
+    selector: Selector,
+    callback: (element: Element) => void,
+    dependencies: DependencyList = []
+) => {
+    useEffect(() => {
+        if (document.querySelector(selector)) return callback(document.querySelector(selector)!);
+            
+        const observer = new MutationObserver(mutations => {
+            if (!document.querySelector(selector)) return;
+        
+            callback(document.querySelector(selector)!);
+            observer.disconnect();
+        });
+
+        observer.observe(document.body, {
+            childList: true,
+            subtree: true
+        });
+    }, dependencies);
+}
